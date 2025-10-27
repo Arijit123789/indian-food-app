3b9e533e3156d86ae3f0d60811af52e88388176e
Arijit123789 <154368328+Arijit123789@users.noreply.github.com>
2025-10-28 00:53:29 +0530

    Update eda.py

--- indian-food-app/modules/eda.py
+++ indian-food-app/modules/eda.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+import pandas as pd  # <-- THIS IS THE FIX
 
 def plot_state_distribution(df):
     fig, ax = plt.subplots()
@@ -9,5 +10,6 @@ def plot_state_distribution(df):
 
 def plot_diet_distribution_by_state(df):
     fig, ax = plt.subplots(figsize=(10, 6))
+    # This line will no longer crash
     pd.crosstab(df['state'], df['diet']).plot(kind='bar', ax=ax)
     st.pyplot(fig)
